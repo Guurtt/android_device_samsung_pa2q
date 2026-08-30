fa8bb4644aa814bd0da499ac9c99d877af930178
Gurt <meowq93@gmail.com>
2026-08-30 19:13:51 +0300

    Create lineage_pa2q.mk

---
+++ lineage_pa2q.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/samsung/e3q/device.mk)
+
+# Inherit from the Lineage configuration.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_pa2q
+PRODUCT_DEVICE := pa2q
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-S936B
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
